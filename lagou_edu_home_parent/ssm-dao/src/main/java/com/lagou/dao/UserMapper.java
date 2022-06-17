dabf6c2848c062c401a2a880d7e8703f3eb0cd82
ZJF-99 <81426121+ZJF-99@users.noreply.github.com>
2022-06-17 20:49:38 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -54,12 +54,16 @@ public interface UserMapper {
     
     
 
-    public void test1();
-    public void test2();
-    public void test3();
-    public void test4();
-    public void test5();
-    public void test6();
+    public void test11();
+    public void test21();
+    public void test31();
+    public void test41();
+    public void test51();
+    public void test61();
+    public void test71();
+    public void test81();
+    public void test91();
+    public void test101();
 
 
 }
